2b8fa1ba6f9667b6619d3cdc45b8bcab9a5ad0a1
PARK JI EUN <pjieuniv20@ewhain.net>
2022-05-11 10:17:55 +0900

    login

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,14 +1,14 @@
-import React, { useState } from 'react';
-import { TextField } from '../components/TextField';
-import { useHistory } from 'react-router-dom';
+import React, { useState } from "react";
+import { TextField } from "../components/TextField";
+import { useHistory } from "react-router-dom";
 import data from "../db/data.json";
+import axios from "axios";
 
 export const LoginPage = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const { replace } = useHistory();
 
-  
   const users = data.users;
 
   const checkUser = () => {
@@ -16,40 +16,60 @@ export const LoginPage = () => {
       alert("아이디와 비밀번호를 입력해주세요");
       return;
     }
-    for(let i = 0; i < users.length; i++) {
-      if (email === users[i].email && password === users[i].password){
-        alert("로그인 성공");
-        replace('/');
-        return;
-      } else if (email === users[i].email && password !== users[i].password) {
-        alert("비밀번호가 틀립니다");
-        return;
-      }
-    }
-    alert("로그인 실패");
-  }
+    axios
+      .post("http://localhost:1337/api/auth/local", {
+        identifier: "user@strapi.io",
+        password: "strapiPassword",
+      })
+      .then((response) => {
+        // Handle success.
+        console.log("Well done!");
+        console.log("User profile", response.data.user);
+        console.log("User token", response.data.jwt);
+      })
+      .catch((error) => {
+        // Handle error.
+        console.log("An error occurred:", error.response);
+      });
+  };
 
   return (
-    <div className="m-4"> 
-    <div className="text-3xl font-bold mb-10">로그인</div>
+    <div className="m-4">
+      <div className="text-3xl font-bold mb-10">로그인</div>
       <div></div>
       <div>
-        <TextField label="이메일" placeholder="이메일을 입력해주세요." value={email} onChange={(event) => {setEmail(event.target.value)}}/>
+        <TextField
+          label="이메일"
+          placeholder="이메일을 입력해주세요."
+          value={email}
+          onChange={(event) => {
+            setEmail(event.target.value);
+          }}
+        />
         <TextField
           label="비밀번호"
           placeholder="비밀번호를 입력해주세요."
           type="password"
           value={password}
-          onChange={(event) => {setPassword(event.target.value)}}
-          />
+          onChange={(event) => {
+            setPassword(event.target.value);
+          }}
+        />
       </div>
 
-      <div className="mt-10"> 
-        <div className="bg-gray-800 text-white text-center py-4 rounded-md" onClick={()=>{checkUser();}}>로그인</div>
+      <div className="mt-10">
+        <div
+          className="bg-gray-800 text-white text-center py-4 rounded-md"
+          onClick={() => {
+            checkUser();
+          }}
+        >
+          로그인
+        </div>
         <div className="text-center py-4 text-gray-800 border border-gray-800">
           회원가입
-          </div>
         </div>
       </div>
+    </div>
   );
 };
